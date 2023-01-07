a2aadb86db18929d713857b5a86b56de9f97af36
Sergeichuk Dmitrii <sergeychukdmitry@gmail.com>
2023-01-07 18:24:20 +0100

    Run all commands in script

--- download_models.sh
+++ download_models.sh
@@ -1,6 +1,6 @@
 # TODO Terminate script if error
 
-# omz_downloader --output_dir vendor/models --name person-detection-0200
+omz_downloader --output_dir vendor/models --name person-detection-0200
 
 mkdir vendor/models/yolov4-tiny
 wget -P vendor/models/yolov4-tiny https://raw.githubusercontent.com/AlexeyAB/darknet/master/cfg/yolov4-tiny.cfg
